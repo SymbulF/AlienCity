99d5c43cf81425d8950f06765e6d16a448b50448
SymbulF <apddef11@hotmail.com>
2016-11-02 19:43:03 -0200

    Projeto_tiro

---
+++ AlienCity-alien_tiro/AlienCity/alien_tiro/Assets/Scripts/Bulletcontrol.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bulletcontrol : MonoBehaviour {
+
+    private Vector2 speed = new Vector2(15, 0);
+    private Rigidbody2D rbBullit;
+
+    void Start()
+    {
+        rbBullit = GetComponent<Rigidbody2D>();
+        rbBullit.velocity = speed * this.transform.localScale.x;
+        Destroy(gameObject, 2f);
+
+    }
+
+
+}
