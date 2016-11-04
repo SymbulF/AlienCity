6d29e3bbd10824b8c76bf20b8b8c395184e97d48
SymbulF <apddef11@hotmail.com>
2016-11-04 07:24:22 -0200

    Sprites Arrumados
    Verificar geração de tiros

--- AlienCity-alien_tiro/AlienCity/alien_tiro/Assets/Scripts/PlayerController.cs
+++ AlienCity-alien_tiro/AlienCity/alien_tiro/Assets/Scripts/PlayerController.cs
@@ -7,76 +7,129 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	private Animator anim;
 	private Rigidbody2D rb2d;
-    public float Velocidade;
-    public bool viradoDireita = true;
-    public float ShootingRate = 0.1f;
-    public float ShootCooldown = 0f;
-    public Transform CreateBullet;
-    public GameObject Bullet;
+	public Transform posPe;
+	[HideInInspector] public bool tocaChao = false;
+	public float Velocidade;
+	public float ForcaPulo = 1000f;
+	[HideInInspector] public bool viradoDireita = true;
+	private bool pula = false; 
+	public float ShootingRate = 0;
+	public float ShootCooldown1 = 0.1f;
+	public float ShootCooldown2 = 0.1f;
+	public Transform CreateBullet;
+	public Transform CreateBullet2;
+	public GameObject Bullet;
+	public GameObject Bullet2;
 
+	
 
 	void Start () {
 		anim = GetComponent<Animator> ();
-		rb2d = GetComponent<Rigidbody2D>();
+		rb2d = GetComponent<Rigidbody2D> ();
 
 	}
-    
-   	
+	
+	// Update is called once per frame
+	void Update () {
+		//Implementar Pulo Aqui!
+		tocaChao = Physics2D.Linecast(transform.position, posPe.position, 1 << LayerMask.NameToLayer("Ground"));
+		if(Input.GetKeyDown(KeyCode.Space) && tocaChao){
+			//pula
+			pula = true;
+		}
+	}
+
 	void FixedUpdate()
 	{
-		if (ShootCooldown > 0)
-			ShootCooldown -= Time.deltaTime; //registra o cooldown do tiro
-
-
 		float translationY = 0;
-		float translationX = Input.GetAxis ("Horizontal") * Velocidade; //Controle de Movimento
+		float translationX = Input.GetAxis ("Horizontal") * Velocidade;
 		transform.Translate(new Vector3(translationX,0,0), Space.World);
 		transform.Rotate (0, 0, 0);
-		if (translationX != 0) {
+		
+		if (translationX != 0 && tocaChao) {
 			anim.SetTrigger ("corre");
 		} else {
 			anim.SetTrigger("parado");
 		}
+
+		//Programar o pulo Aqui!
+		if(pula == true){
+
+			rb2d.AddForce(new Vector2(0f, ForcaPulo));
+			anim.SetTrigger("pula");
+			pula = false;		
+		
+		} 
 
 		if (translationX > 0 && !viradoDireita) {
 			Flip ();
 		} else if (translationX < 0 && viradoDireita) {
 			Flip();
 		}
-        
-        if(Input.GetKeyDown(KeyCode.Space)) // Executa tiro com tecla espaço
+		
+		
+		if(Input.GetKeyDown(KeyCode.C)) // Executa tiro com tecla c
         {
-            anim.SetTrigger("Atirar");
-            Fire();
-			ShootCooldown = ShootingRate;
+            anim.SetTrigger("atira1");
+            Fire1();
+			ShootCooldown1 = ShootingRate;   
+        
+        
+        }  
+		
+		if(Input.GetKeyDown(KeyCode.V)) // Executa tiro com tecla v
+        {
+            anim.SetTrigger("atira2");
+            Fire2();
+			ShootCooldown2 = ShootingRate;
         
         
         
         }  
+		
 
 	}
 	
-    void Flip() // verifica a escala do personagem e gerencia a virada do personagem
+	void Flip()
 	{
 		viradoDireita = !viradoDireita;
 		Vector3 escala = transform.localScale;
 		escala.x *= -1;
 		transform.localScale = escala;
 	}
-    
-    void Fire()
+	
+	void Fire1()
     {
         
-        if(ShootCooldown <= 0f) //verifica se o cooldown do tiro está zerado
+        if(ShootCooldown1 <= 0f) //verifica se o cooldown do tiro está zerado
         {
             if(Bullet != null) //verifica a existência de um prefab "bala"
             {
-                var cloneBullet = Instantiate(Bullet, CreateBullet.position, Quaternion.identity) as GameObject;
-                cloneBullet.transform.localScale = this.transform.localScale; //cria um clone do objeto "tiro" toda vez que o botão de atirar for apertado
+                var cloneBullet1 = Instantiate(Bullet, CreateBullet.position, Quaternion.identity) as GameObject;
+                cloneBullet1.transform.localScale = this.transform.localScale; //cria um clone do objeto "tiro" toda vez que o botão de atirar for apertado
+                  
+            }
+              
+        }  
+    
+    }
+
+	void Fire2()
+    {
+        
+        if(ShootCooldown2 <= 0f) //verifica se o cooldown do tiro está zerado
+        {
+            if(Bullet != null) //verifica a existência de um prefab "bala"
+            {
+                var cloneBullet2 = Instantiate(Bullet2, CreateBullet2.position, Quaternion.identity) as GameObject;
+                cloneBullet2.transform.localScale = this.transform.localScale; //cria um clone do objeto "tiro" toda vez que o botão de atirar for apertado
                   
             }
               
         }  
     
     }	
+
+	
+	
 }
